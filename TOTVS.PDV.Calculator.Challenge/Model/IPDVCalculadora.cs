4a2b7b190df10fee91c1ba72521ba4570c998e89
Leonardo Honório <iblue.loliveira@mag.terceiros.com.br>
2021-05-25 19:49:09 -0300

    Add project files.

---
+++ TOTVS.PDV.Calculator.Challenge/Model/IPDVCalculadora.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TOTVS.PDV.Calculator.Challenge.Model
+{
+    interface IPDVCalculadora
+    {
+
+        public string ObterTroco(Operacao op);
+    
+    }
+}
---
+++ TOTVS.PDV.Calculator.Challenge/Model/Operacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TOTVS.PDV.Calculator.Challenge.Model
+{
+    public class Operacao
+    {
+        public int? OperacaoId;
+
+        public long? ValorTotal;
+
+        public long? ValorPago;
+
+        public string NomeOperador; 
+    }
+}
